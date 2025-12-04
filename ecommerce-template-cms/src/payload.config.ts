8be91f7c409d68401abf3e9e1c7d3f3e6874646c
Juan Reyes <juanisoul@gmail.com>
2025-12-04 18:28:57 -0400

    Add optional cloudflare r2 payload storage

--- ecommerce-template-cms/src/payload.config.ts
+++ ecommerce-template-cms/src/payload.config.ts
@@ -2,6 +2,7 @@
 import { postgresAdapter } from '@payloadcms/db-postgres'
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import { s3Storage } from '@payloadcms/storage-s3'
 import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
@@ -37,6 +38,23 @@ export default buildConfig({
   sharp,
   plugins: [
     payloadCloudPlugin(),
-    // storage-adapter-placeholder
+    ...(process.env.S3_BUCKET
+      ? [
+          s3Storage({
+            collections: {
+              media: true, // Apply storage to 'media' collection
+            },
+            bucket: process.env.S3_BUCKET || '',
+            config: {
+              credentials: {
+                accessKeyId: process.env.S3_ACCESS_KEY_ID || '',
+                secretAccessKey: process.env.S3_SECRET || '',
+              },
+              region: 'auto', // Cloudflare R2 uses 'auto' as the region
+              endpoint: process.env.S3_ENDPOINT || '',
+            },
+          }),
+        ]
+      : []),
   ],
 })
